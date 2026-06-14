5e2c25d135ec96e2a5782df02076c6a6b3bc03aa
zeinasherif002 <zeina.sherif@adm-medsofts.com>
2026-06-14 10:17:40 +0000

    fix base path for Netlify

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Adm-Medsofts-Commodities/',
   plugins: [react()],
+  base: '/',
 })
